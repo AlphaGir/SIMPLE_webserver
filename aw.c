1d512d781e3103a401412794a2857619c23b574b
Lordmiao <1534594096@qq.com>
2019-08-07 15:49:23 +0800

    我会不会饿死啊

--- aw.c
+++ aw.c
@@ -1,9 +1,11 @@
 #include"aw.h"
 #include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
 /*
  
  */
-int err(int code,char *m);
+int err(int code,char m[]);
 void doit(char* url,int *code,char *m,char *sendmsg)//获取返回的响应报文
 {
    int i;
@@ -16,22 +18,32 @@ void doit(char* url,int *code,char *m,char *sendmsg)//获取返回的响应报�
    char v2[10];
    char value1[100];
    char value2[100];
+   char seg[10];
    sscanf(url,"%s %s %s ",method,uri,version);
    if(strcmp(method,"GET")!=0&&strcmp(method,"POST")!=0)
      {
-       *code=err(4,m);
-       exit(1);
+       printf("[[[[[[\n");
+       *code=err(4,seg);
+       //printf("%s",seg);
+       strcpy(m,seg);
+
+       //printf("%s\n",m);
+       return;
      }
    else
      {
+	                
             if(!strstr(uri,"cgi"))//静态资源
             {
-              strcat(path,uri);
-              getstatic(path,sendmsg,*code,m);
+              printf("----------\n");
+              strcpy(path,uri);
+	      printf("%s",path);
+     //         getstatic(path,sendmsg,*code,m);
             }
             else
             {
                sscanf(uri,"%s?%s=%s&%s=%s",path,v1,value1,v2,value2);
+	       printf("%s %s %s %s %s",path,v1,value1,v2,value2);
                getdynamic(path,v1,value1,v2,value2,sendmsg,*code,m);
             }
      }
@@ -52,19 +64,21 @@ int err(int f,char *ret)
 {
    switch(f)
    {
-     case 1: f=200,strcpy(ret,"ok");break;
-     case 2: f=404,strcpy(ret,"not found");break;
-     case 3: f=403,strcpy(ret,"fobbiden permission");break;
-     case 4: f=501,strcpy(ret,"not implement");break;
+     case 1: f=200;strcpy(ret,"ok");break;
+     case 2: f=404;strcpy(ret,"not found");break;
+     case 3: f=403;strcpy(ret,"fobbiden permission");break;
+     case 4: f=501;strcpy(ret,"not implement");break;
    }
-   printf("%d",f);
+   //printf("%d",f);
    return f;
 }
 int main()
 {
-   char *url="DELETE / HTTP/1.1\r\nContent-Type:text/html\r\nContent-Length: 11\r\nServer: mengkang\r\n\r\nhello world";
+   char *url="delete / HTTP/1.1\r\nContent-Type:text/html\r\nContent-Length: 11\r\nServer: mengkang\r\n\r\nhello world";
    int m;
-   char *msg;
-   doit(url,&m,msg);
+   char msg[20];
+   bzero(msg,20);
+   char *sendmsg;
+   doit(url,&m,msg,sendmsg);
    printf("%d %s",m,msg);
 }
--- aw.h
+++ aw.h
@@ -1,7 +1,7 @@
 #ifndef _AW_H_
 #define _AW_H_
 int err(int code,char *m);
-void doit(char *url,int *code,char *m);
-void handle_static(char *msg);
-void handle_dynamic(char *msg);
+void doit(char *url,int *code,char *m,char *sendmsg);
+void getstatic(char *uri,char *sendmsg,int *code,char *m);
+void getdynamic(char *uri,char *v1,char *value1,char *v2,char *value2,char *sendmsg,int *code,char *m);
 #endif
