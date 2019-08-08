78274baf3d5318c68e31f8224d57034c1bec698a
Lordmiao <1534594096@qq.com>
2019-08-08 15:01:11 +0800

    巴拉巴拉,测试动态网页文件去

--- aw.c
+++ aw.c
@@ -1,20 +1,51 @@
 #include"aw.h"
 /*
- 恶心的操作
+ AW OPERATION
+ EW OPERATION
  */
 
-int err(int code,char m[]);
+void cutout(char *uri,int begin,int end,char *ret)
+{
+     int i=0;
+     int j=0;
+     int s,e;
+     for(;i<strlen(uri);i++)
+     {
+         if(uri[i]==begin)
+         {
+                 s=i;
+         }
+         if(uri[i]==end&&end!='\0')
+          {
+                  e=i;
+                  if(s<e)
+                  break;
+          }
+     }
+     if(end=='\0')
+     {
+	     int len=strlen(uri)-s;
+	     char ss[len];
+	     memcpy(ret,uri+s+1,len-1);
+	     ret[len]='\0';//EW   傻逼代码 
+     }
+     else
+     {
+          int len=e-s;
+          memcpy(ret,uri+s+1,len-1);
+          ret[len]='\0';
+     }
+
+ }
+
 void doit(char* url)//获取返回的响应报文
 {
    int i;
-   char uri[50];
+   char uri[100];
    char method[5];
-   char version[5];
-   char v1[10];
-   char v2[10];
-   char value1[100];
-   char value2[100];
-   sscanf(url,"%s %s %s ",method,uri,version);
+   char version[10];
+   char p1[300]="./";
+   sscanf(url,"%s %s %s",method,uri,version);
    printf("uri:%s method:%s version:%s\n",uri,method,version);
    if(strcmp(method,"GET")!=0&&strcmp(method,"POST")!=0)
      {
@@ -32,27 +63,44 @@ void doit(char* url)//获取返回的响应报文
             }
             else
             {
-               sscanf(uri,"%s?%s=%s&%s=%s",path,v1,value1,v2,value2);
-	       printf("path:%s v1:%s value1:%s v2:%s value2:%s",path,v1,value1,v2,value2);
-               //getdynamic(path,v1,value1,v2,value2,sendmsg,*code,m);
+	     cutout(uri,'/','?',path);
+             strcat(p1,path);
+	     strcpy(path,p1);
+             cutout(uri,'?','=',v1);
+             cutout(uri,'=','&',value1);
+             cutout(uri,'&','=',v2);
+             cutout(uri,'=','\0',value2);
+	     printf("path:%s v1:%s value1:%s v2:%s value2:%s",path,v1,value1,v2,value2);//value2傻逼玩意儿
+             getdynamic();
             }
      }
 }
-//void getdynamic(char *uri,char *v1,char *value1,char *v2,char *value2,char *sendmsg,int *code,char *m)
-//{
-     
-//}
+void getdynamic()
+{
+    if(stat(path,&sbuf)<0)
+    {
+                codetype=err(2,msg);
+                return;
+    }
+
+    if(!(S_ISREG(sbuf.st_mode))||!(S_IXUSR&sbuf.st_mode))
+    {
+                codetype=err(3,msg);
+		return;
+    }
+
+}
 void getstatic()
 {
          //建议资源有没有 没有404 然后可不可以访问
          //不可以就 403
          //有&&可以访问 读取资源返回字符串
-        int fd;   
+     /*   int fd;   
 	char buff[4096];
 	char ss[1024];
 	char temp[10];
 	int is_static;
-	struct stat sbuf;
+	struct stat sbuf;*/
 	if(strcmp(path,"/")==0)
 	{
 		if((fd=open("index.html",O_RDONLY))!=-1)
@@ -81,8 +129,6 @@ void getstatic()
 		codetype=err(1,msg);
 		return;
 	}
-
-
 }
 int err(int f,char *ret)
 {
--- aw.h
+++ aw.h
@@ -12,9 +12,20 @@ int codetype;//编码
 char sendmsg[4096];//发送消息
 char msg[20];//编码解释
 char path[100];//路径
+char v1[100];
+char value1[100];
+char v2[100];
+char value2[100];
+
+        int fd;
+        char buff[4096];
+        int is_static;
+        struct stat sbuf;
+
 
 int err(int code,char *m);
 void doit(char *url);
 void getstatic();
 void getdynamic();
+void cutout(char *uri,int begin,int end,char *ret);
 #endif
